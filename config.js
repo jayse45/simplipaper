f251851a76fd6f44c88f80a3ac13d008ee36a00a
jayse45 <ngyasi0@gmail.com>
2022-12-21 13:44:00 +0000

    react wallpaper

---
+++ config.js
@@ -0,0 +1,24 @@
+export const wallpapers = [
+    {
+      id: '1',
+      name: 'Wallpaper 1',
+      imageUrl: 'https://unsplash.com/photos/0lZqk6JPdQE',
+    },
+    {
+      id: '2',
+      name: 'Wallpaper 2',
+      imageUrl: 'https://unsplash.com/photos/oOiPIRQnEPs',
+    },
+    {
+        id: '3',
+        name: 'Wallpaper 2',
+        imageUrl: 'https://unsplash.com/photos/J-Gps4WTMAQ',
+    },
+    {
+        id: '4',
+        name: 'Wallpaper 2',
+        imageUrl: 'https://unsplash.com/photos/REOlPVGPm3E',
+    },
+    // ... more wallpapers
+  ];
+  
